9e23bebe1f45f2dbf72e1df5ae10981ab5c947e5
Sina Nazari <sinanazari3000@gmail.com>
2022-06-30 18:31:00 +0430

    master:Add New Source in Project

---
+++ Portfolio/Models/ContactModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio.Models
+{
+    public class ContactModel
+    {
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string WebSite { get; set; }
+    }
+
+
+}
