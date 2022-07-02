8e34a1297df0ee40539dfab6fe62b2e0211daff1
Sina Nazari <sinanazari3000@gmail.com>
2022-07-02 10:21:48 +0430

    Master:Update And Edit Project

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -11,9 +11,31 @@ namespace Portfolio.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<PortfolioModel> _portfolios = new List<PortfolioModel>()
+        {
+            new PortfolioModel
+            {
+                Id =1,Title="First_Photo",Image="01.jpg",Description="This is first Photo"
+            },
+            new PortfolioModel
+            {
+                Id =2,Title="Tow_Photo",Image="02.jpg",Description="This is Tow Photo"
+            },
+            new PortfolioModel
+            {
+                Id =3,Title="Three_Photo",Image="03.jpg",Description="This is There Photo"
+            },
+            new PortfolioModel
+            {
+                Id =4,Title="Fore_Photo",Image="04.jpg",Description="This is Fore Photo"
+            },
+        };
+
         public IActionResult Index()
         {
-            return View();
+            ViewData["headingTitle"] = "Welcome to My WebSite";
+            ViewData["headingText"] = "Hi My Name is Sina Nazari I A Programmer";
+            return View(_portfolios);
         }
 
         public IActionResult Contact()
---
+++ Portfolio/Models/PortfolioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Portfolio.Models
+{
+    public class PortfolioModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+    }
+}
